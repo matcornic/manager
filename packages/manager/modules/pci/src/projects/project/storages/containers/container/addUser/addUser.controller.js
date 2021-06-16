f8e87cbf8e68e0a51b084836862f5977da283bad
jjuret <julien.juret.ext@ovhcloud.com>
2021-06-16 15:40:48 +0200

    feat(container): add user popup
    Signed-off-by: jjuret <julien.juret.ext@ovhcloud.com>

---
+++ packages/manager/modules/pci/src/projects/project/storages/containers/container/addUser/addUser.controller.js
@@ -0,0 +1,46 @@
+import get from 'lodash/get';
+
+export default class PciBlockStorageDetailsAddUserController {
+  /* @ngInject */
+  constructor($translate, CucCloudMessage, PciProjectStorageContainersService) {
+    this.$translate = $translate;
+    this.CucCloudMessage = CucCloudMessage;
+    this.PciProjectStorageContainersService = PciProjectStorageContainersService;
+  }
+
+  $onInit() {
+    this.isLoading = false;
+  }
+
+  deleteStorage() {
+    this.isLoading = true;
+    return this.PciProjectStorageContainersService.deleteContainer(
+      this.projectId,
+      this.container,
+    )
+      .then(() =>
+        this.goBack(
+          this.$translate.instant(
+            'pci_projects_project_storages_containers_container_delete_success_message',
+            {
+              container: this.container.name,
+            },
+          ),
+        ),
+      )
+      .catch((err) =>
+        this.goBack(
+          this.$translate.instant(
+            'pci_projects_project_storages_containers_container_delete_error_delete',
+            {
+              message: get(err, 'data.message', null),
+            },
+          ),
+          'error',
+        ),
+      )
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
+}
